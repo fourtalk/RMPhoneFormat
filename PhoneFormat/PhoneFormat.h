27d075d36e014ebaaf628d1ff5c08388f60273b2
Сергей Галездинов <segazero@gmail.com>
2016-05-23 01:36:44 +0300

    fixed umbrella header

---
+++ PhoneFormat/PhoneFormat.h
@@ -0,0 +1,19 @@
+//
+//  PhoneFormat.h
+//  PhoneFormat
+//
+//  Created by Sergey Galezdinov on 23.05.16.
+//  Copyright © 2016 Rick Maddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhoneFormat.
+FOUNDATION_EXPORT double PhoneFormatVersionNumber;
+
+//! Project version string for PhoneFormat.
+FOUNDATION_EXPORT const unsigned char PhoneFormatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhoneFormat/PublicHeader.h>
+
+#import "RMPhoneFormat.h"
